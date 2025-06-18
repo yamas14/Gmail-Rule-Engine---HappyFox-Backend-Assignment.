d44df5017f92cfe5b6be356bb7d7a11d8e2a0daa
yamas14 <saisamay80@gmail.com>
2025-06-19 03:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+##Gmail API configuration
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 CREDENTIALS_FILE = 'credentials.json'
 TOKEN_FILE = 'token.json'
-
+#PostgreSQL database configuration
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost:5432/gmail_rules')
